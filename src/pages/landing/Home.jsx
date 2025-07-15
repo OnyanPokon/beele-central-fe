fb26be67eea02086e57820fb1112aa5162bf6c14
OnyanPokon <mohamadrafiqd@gmail.com>
2025-07-15 22:17:07 +0800

    feat: creating base ui page for testimonial

--- src/pages/landing/Home.jsx
+++ src/pages/landing/Home.jsx
@@ -1,6 +1,7 @@
-import { Reveal } from '@/components';
+import { FormItem, Reveal } from '@/components';
+import { InputType } from '@/constants';
 import { CheckCircleFilled, ShopOutlined, ShoppingCartOutlined } from '@ant-design/icons';
-import { Avatar, Button, Typography } from 'antd';
+import { Avatar, Button, Card, Form, Typography } from 'antd';
 import gsap from 'gsap';
 import { useLayoutEffect, useRef } from 'react';
 
@@ -33,6 +34,33 @@ const Home = () => {
 
     return () => ctx.revert();
   }, []);
+
+  const formFields = () => [
+    {
+      label: `Nama`,
+      name: 'nama',
+      type: InputType.TEXT,
+      rules: [
+        {
+          required: true,
+          message: `Nama harus diisi`
+        }
+      ],
+      size: 'large'
+    },
+    {
+      label: `Testimonial`,
+      name: 'testimonial',
+      type: InputType.LONGTEXT,
+      rules: [
+        {
+          required: true,
+          message: `Nama harus diisi`
+        }
+      ],
+      size: 'large'
+    }
+  ];
 
   return (
     <>
@@ -98,7 +126,114 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="min-h-screen w-full"></section>
+      <section className="mx-auto flex w-full max-w-screen-xl flex-col gap-y-10 px-6 py-32">
+        <div className="">
+          <Typography.Title level={3} style={{ color: '#142b71' }}>
+            <Reveal>Bagaimana pendapat orang tentang Belee?</Reveal>
+          </Typography.Title>
+        </div>
+        <div className="grid grid-cols-6 gap-4">
+          <Card className="relative col-span-2 bg-gray-100/50">
+            <div className="mt-4 flex flex-col gap-y-2">
+              <Typography.Title level={5} style={{ color: '#142b71' }}>
+                <Reveal>Mohamad Rafiq Daud (Owner of UMKM GO)</Reveal>
+              </Typography.Title>
+              <p className="text-secondary-200">
+                <Reveal>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Reveal>
+              </p>
+            </div>
+            <Avatar src="/image_asset/figure/user.png" className="absolute -top-4 left-6 border border-gray-200" size="large" style={{ backgroundColor: '#fff', color: '#396396' }} />
+          </Card>
+          <Card className="relative col-span-2 bg-gray-100/50">
+            <div className="mt-4 flex flex-col gap-y-2">
+              <Typography.Title level={5} style={{ color: '#142b71' }}>
+                <Reveal>Mohamad Rafiq Daud (Owner of UMKM GO)</Reveal>
+              </Typography.Title>
+              <p className="text-secondary-200">
+                <Reveal>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Reveal>
+              </p>
+            </div>
+            <Avatar src="/image_asset/figure/user.png" className="absolute -top-4 left-6 border border-gray-200" size="large" style={{ backgroundColor: '#fff', color: '#396396' }} />
+          </Card>
+          <Card className="relative col-span-2 bg-gray-100/50">
+            <div className="mt-4 flex flex-col gap-y-2">
+              <Typography.Title level={5} style={{ color: '#142b71' }}>
+                <Reveal>Mohamad Rafiq Daud (Owner of UMKM GO)</Reveal>
+              </Typography.Title>
+              <p className="text-secondary-200">
+                <Reveal>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Reveal>
+              </p>
+            </div>
+            <Avatar src="/image_asset/figure/user.png" className="absolute -top-4 left-6 border border-gray-200" size="large" style={{ backgroundColor: '#fff', color: '#396396' }} />
+          </Card>
+        </div>
+        <hr />
+        <div className="grid grid-cols-6 gap-4">
+          <Card className="col-span-2 bg-gray-100/50">
+            <Typography.Title level={5}>
+              <Reveal>Belee In Your Bussiness</Reveal>
+            </Typography.Title>
+            <p className="mt-4 text-xs">
+              <Reveal>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua.
+              </Reveal>
+            </p>
+          </Card>
+          <Card
+            style={{
+              backgroundImage: `url('/image_asset/background/hero_bg.png')`,
+              backgroundPosition: 'center',
+              backgroundSize: 'cover'
+            }}
+            className="col-span-4 text-white"
+          >
+            <div className="flex flex-col gap-y-2 p-4">
+              <Typography.Title level={5} style={{ color: '#fff' }}>
+                <Reveal color="#fff">Tunggu Apa Lagi?</Reveal>
+              </Typography.Title>
+              <p className="max-w-lg text-xs">
+                <Reveal color="#fff">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                </Reveal>
+              </p>
+              <Button className="mt-4 w-fit">Daftar Sebagai Mitra</Button>
+            </div>
+          </Card>
+        </div>
+      </section>
+      <section
+        style={{
+          backgroundImage: `url('/image_asset/background/testimonial_bg.png')`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover'
+        }}
+      >
+        <div className="mx-auto flex min-h-screen w-full max-w-screen-md flex-col items-center justify-center gap-y-12 px-6 py-28">
+          <Typography.Title level={3} style={{ color: '#fff' }}>
+            Kirim Tetimoni dan Feedback
+          </Typography.Title>
+          <div
+            style={{
+              backgroundImage: `url('/image_asset/background/member_bg.png')`,
+              backgroundPosition: 'center',
+              backgroundSize: 'cover'
+            }}
+            className="w-full rounded-xl bg-white shadow-lg"
+          >
+            <Form layout="vertical" className="flex h-full flex-col justify-between p-12">
+              <div className="mb-4 flex flex-1 flex-col gap-y-4 overflow-y-auto">
+                <FormItem formFields={formFields()} />
+              </div>
+              <div className="flex flex-col gap-y-4">
+                <Button size="large" variant="solid" color="primary">
+                  Berikutnya
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
